01e43ae6a5f86ae9580345362c3dee3f6954c377
Daniel Cousens <github@dcousens.com>
2014-10-06 22:13:26 +1100

    tests: move all test data to fixtures

--- test/wallet.js
+++ test/wallet.js
@@ -1,40 +1,32 @@
 var assert = require('assert')
-
 var bitcoinjs = require('bitcoinjs-lib')
-var networks = bitcoinjs.networks
 
 var Wallet = require('../src/wallet')
 
-function fakeHash(i) {
-  return bitcoinjs.crypto.sha256('' + i)
-}
-
-function fakeHashHex(i) {
-  var hash = fakeHash(i)
-  Array.prototype.reverse.call(hash)
-  return hash.toString('hex')
-}
+var fixtures = require('./fixtures/wallet.json')
 
 describe('Wallet', function() {
-  var seed
+  var f, wallet
+
   beforeEach(function() {
-    seed = bitcoinjs.crypto.sha256("don't use a string seed like this in real life")
+    f = fixtures.valid[0]
+
+    var seed = new Buffer(f.seed, 'hex')
+    var network = bitcoinjs.networks[f.network]
+    wallet = new Wallet(seed, network)
   })
 
   describe('constructor', function() {
-    var wallet
-    beforeEach(function() {
-      wallet = new Wallet(seed)
-    })
-
     it('defaults to Bitcoin network', function() {
-      assert.equal(wallet.network, networks.bitcoin)
+      var defaulted = new Wallet(new Buffer(32))
+
+      assert.equal(defaulted.network, bitcoinjs.networks.bitcoin)
     })
 
     it('uses the network if specified', function() {
-      wallet = new Wallet(seed, networks.testnet)
+      var expected = bitcoinjs.networks[f.network]
 
-      assert.equal(wallet.network, networks.testnet)
+      assert.equal(wallet.network, expected)
     })
 
     it("generates m/0'/0 as the external chain node", function() {
@@ -50,62 +42,18 @@ describe('Wallet', function() {
     })
   })
 
-  describe('createTransaction', function() {
-    var unspents, wallet
-
-    beforeEach(function() {
-      unspents = [
-        {
-          "txId": fakeHashHex(6),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 20000
-        },
-        {
-          "txId": fakeHashHex(7),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n2fiWrHqD6GM5GiEqkbWAc6aaZQp3ba93X',
-          "vout": 1,
-          "value": 30000
-        },
-        {
-          "txId": fakeHashHex(8),
-          "blockHash": null,
-          "blockHeight": null,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 6000
-        }
-      ]
-
-      wallet = new Wallet(seed, networks.testnet)
-    })
-  })
+  describe.skip('createTransaction', function() {})
 
   describe('generateAddress', function() {
-    var wallet
-
-    beforeEach(function() {
-      wallet = new Wallet(seed, networks.testnet)
-    })
-
     it('generates a new external (and internal) Address', function() {
       assert.deepEqual(wallet.getAddresses(), [
-        "n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa",
-        "mnXiDR4MKsFxcKJEZjx4353oXvo55iuptn"
+        f.addresses[0],
+        f.addresses[f.addresses.length / 2]
       ])
 
       wallet.generateAddress()
 
-      assert.deepEqual(wallet.getAddresses(), [
-        'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-        'n2fiWrHqD6GM5GiEqkbWAc6aaZQp3ba93X',
-        'mnXiDR4MKsFxcKJEZjx4353oXvo55iuptn',
-        'mtQrg4dcAVhDDzbyXawmRbpzRTRiUgfAE5'
-      ])
+      assert.deepEqual(wallet.getAddresses(), f.addresses.slice(0, 4))
     })
 
     it('returns the latest external Address', function() {
@@ -117,144 +65,42 @@ describe('Wallet', function() {
 
   describe.skip('getAddress', function() {})
   describe('getAddresses', function() {
-    var wallet
+    it.only('returns all known addresses', function() {
+      for (var i = 2; i < f.addresses.length; i += 2) {
+        wallet.generateAddress()
+      }
 
-    beforeEach(function() {
-      wallet = new Wallet(seed, networks.testnet)
-    })
-
-    it('returns all known addresses', function() {
-      wallet.generateAddress()
-
-      assert.deepEqual(wallet.getAddresses(), [
-        'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-        'n2fiWrHqD6GM5GiEqkbWAc6aaZQp3ba93X',
-        'mnXiDR4MKsFxcKJEZjx4353oXvo55iuptn',
-        'mtQrg4dcAVhDDzbyXawmRbpzRTRiUgfAE5'
-      ])
+      assert.deepEqual(wallet.getAddresses(), f.addresses)
     })
   })
 
   describe('getBalance', function() {
-    var unspents, wallet
-
     beforeEach(function() {
-      unspents = [
-        {
-          "txId": fakeHashHex(6),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 20000
-        },
-        {
-          "txId": fakeHashHex(7),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n2fiWrHqD6GM5GiEqkbWAc6aaZQp3ba93X',
-          "vout": 1,
-          "value": 30000
-        },
-        {
-          "txId": fakeHashHex(8),
-          "blockHash": null,
-          "blockHeight": null,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 6000
-        }
-      ]
-
-      wallet = new Wallet(seed)
-      wallet.setUnspentOutputs(unspents)
+      wallet.setUnspentOutputs(f.unspents)
     })
 
     it('sums all unspents', function() {
-      assert.equal(wallet.getBalance(), 56000)
+      assert.equal(wallet.getBalance(), 56000) // FIXME: move balance to fixtures
     })
   })
 
   describe.skip('getChangeAddress', function() {})
 
   describe('getConfirmedBalance', function() {
-    var unspents, wallet
-
     beforeEach(function() {
-      unspents = [
-        {
-          "txId": fakeHashHex(6),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 20000
-        },
-        {
-          "txId": fakeHashHex(7),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n2fiWrHqD6GM5GiEqkbWAc6aaZQp3ba93X',
-          "vout": 1,
-          "value": 30000
-        },
-        {
-          "txId": fakeHashHex(8),
-          "blockHash": null,
-          "blockHeight": null,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 6000
-        }
-      ]
-
-      wallet = new Wallet(seed)
-      wallet.setUnspentOutputs(unspents)
+      wallet.setUnspentOutputs(f.unspents)
     })
 
     it('sums confirmed unspents', function() {
-      assert.equal(wallet.getConfirmedBalance(), 50000)
+      assert.equal(wallet.getConfirmedBalance(), 50000) // FIXME: move balance to fixtures
     })
   })
 
   describe('setUnspentOutputs', function() {
-    var unspents, wallet
-
-    beforeEach(function() {
-      unspents = [
-        {
-          "txId": fakeHashHex(6),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 20000
-        },
-        {
-          "txId": fakeHashHex(7),
-          "blockHash": fakeHashHex(0),
-          "blockHeight": 298368,
-          "address" : 'n2fiWrHqD6GM5GiEqkbWAc6aaZQp3ba93X',
-          "vout": 1,
-          "value": 30000
-        },
-        {
-          "txId": fakeHashHex(8),
-          "blockHash": null,
-          "blockHeight": null,
-          "address" : 'n1GyUANZand9Kw6hGSV9837cCC9FFUQzQa',
-          "vout": 0,
-          "value": 6000
-        }
-      ]
-
-      wallet = new Wallet(seed)
-    })
-
     it('sets wallet.unspents correctly', function() {
-      wallet.setUnspentOutputs(unspents)
+      wallet.setUnspentOutputs(f.unspents)
 
-      assert.equal(wallet.unspents, unspents)
+      assert.equal(wallet.unspents, f.unspents)
     })
 
     // TODO: test validation
